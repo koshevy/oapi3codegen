35e336b104aab028b7e834aa0e92ccb3f468772e
Alexander Koshevy <koshevy@gmail.com>
2018-05-09 17:49:03 +0400

    Testing cli bin in environment

--- bin/oapi3codegen.js
+++ bin/oapi3codegen.js
@@ -1,11 +1,37 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const execa = require('execa');
+const fsExtra = require('fs-extra');
+const _ = require('lodash');
+const { getArvgParam } = require('../lib');
 
-console.log(process);
-console.log('———');
+const params = _.mapValues({
+    srcPath: undefined,
+    destPath: undefined,
+    separatedFiles: false
+  },
+  (v, k) => getArvgParam(k) || v
+);
 
-// создание компонента
+const paramsString = _.compact(_.map(
+  params,
+  (v, k) => (v !== undefined) ? `--${k} ${v}` : null
+)).join(' ');
+
+const cliTsPath = path.resolve(__dirname, '../dist/cli.js');
+
+// first time execution: install packages and compile TS sources
+if (!fsExtra.pathExistsSync(cliTsPath)) {
+  execa.shellSync([
+    'cd ./node_modules/oapi3codegen',
+    'npm install',
+    'tsc'
+  ].join(' && '), {stdout: 'inherit'});
+}
+
+// execute CLI interface
 execa.shellSync([
-  `tsc && node ./dist/cli.js`
-].join(' && '), {stdout: 'inherit'});
+  'cd ./node_modules/oapi3codegen',
+  `node ./dist/cli.js`
+].join(' && '));
